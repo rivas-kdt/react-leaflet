3a73b8d244ec52babc0cb6e021b6d713a7c0a5e4
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-02-06 10:51:57 +0800

    marker on maps

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -8,10 +8,13 @@ import {
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
 import { useState } from "react";
+import "../App.css";
 
 function Map() {
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
-  const [markers, setMarkers] = useState([[14.533742443483478, 120.98137642209622]]);
+  const [markers, setMarkers] = useState([
+    [14.533742443483478, 120.98137642209622],
+  ]);
   const customIcon = new Icon({
     iconUrl: "marker-icon2.svg",
     iconSize: [38, 38],
@@ -40,8 +43,8 @@ function Map() {
   console.log(markers);
 
   return (
-    <div className=" h-[calc(100vh-60px)] w-screen">
-      <MapContainer center={[14.539463420883909, 120.9830204750233]} zoom={20}>
+    <div className=" main">
+      <MapContainer center={[14.539463420883909, 120.9830204750233]} zoom={18}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -58,14 +61,14 @@ function Map() {
           </Marker>
         ))}
         {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
-        <Popup>
-          <div className=" flex flex-col px-4 py-2 justify-center items-center">
-            <img src="vite.svg" className=" w-[150px] mb-4"></img>
-            <p>This is a popup</p>
-            <a href="https://react-leaflet.js.org/">Go to link!</a>
-          </div>
-        </Popup>
-      </Marker> */}
+      <Popup>
+        <div className=" flex flex-col px-4 py-2 justify-center items-center">
+          <img src="vite.svg" className=" w-[150px] mb-4"></img>
+          <p>This is a popup</p>
+          <a href="https://react-leaflet.js.org/">Go to link!</a>
+        </div>
+      </Popup>
+    </Marker> */}
         <LocationMarker />
       </MapContainer>
     </div>
